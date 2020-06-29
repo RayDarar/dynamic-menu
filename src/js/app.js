80ce308831b57f1ba0939e2a53bdfdc8154ef1f2
RayDarar <dfqgth400@gmail.com>
2020-06-30 00:26:47 +0600

    finishing routing

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ const Classes = {
   PAGE_WRAPPED: "content__page_wrapped",
   BURGER: "burger",
   BURGER_ACTIVE: "burger_active",
+  HEADER: "header",
+  HEADER_ACTIVE: "header_active",
+  NAV_ITEM: "nav-item",
 };
 
 const pages = {
@@ -11,12 +14,13 @@ const pages = {
   current: document.querySelector("." + Classes.PAGE_ACTIVE),
 };
 const burger = document.querySelector("." + Classes.BURGER);
+const header = document.querySelector("." + Classes.HEADER);
+const navItems = document.querySelectorAll("." + Classes.NAV_ITEM);
 
 function toggleWrap() {
-  // toggling burger state
+  // toggling states
   burger.classList.toggle(Classes.BURGER_ACTIVE);
-
-  // toggling current page state
+  header.classList.toggle(Classes.HEADER_ACTIVE);
   pages.current.classList.toggle(Classes.PAGE_WRAPPED);
 
   // if wrapped, add listener to unwrap him
@@ -25,4 +29,20 @@ function toggleWrap() {
   } else pages.current.removeEventListener("click", toggleWrap);
 }
 
+function selectPage(index) {
+  const page = pages.all[index];
+  
+  toggleWrap();
+  pages.current.classList.remove(Classes.PAGE_ACTIVE);
+  page.classList.add(Classes.PAGE_ACTIVE);
+  pages.current = page;
+}
+
 burger.onclick = toggleWrap;
+
+for (const navItem of navItems) {
+  navItem.onclick = () => {
+    const index = parseInt(navItem.dataset.index);
+    selectPage(index);
+  };
+}
