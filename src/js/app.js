f8ca40088d0dff6cbb4be8e6694f4d209e952b9d
RayDarar <dfqgth400@gmail.com>
2020-06-30 00:01:50 +0600

    burger done

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,20 @@ const Classes = {
   PAGE: "content__page",
   PAGE_ACTIVE: "content__page_active",
   PAGE_WRAPPED: "content__page_wrapped",
+  BURGER: "burger",
+  BURGER_ACTIVE: "burger_active",
 };
 
 const pages = {
   all: document.querySelectorAll("." + Classes.PAGE),
   current: document.querySelector("." + Classes.PAGE_ACTIVE),
 };
+const burger = document.querySelector("." + Classes.BURGER);
 
-// setting click event for wrapping
-for (const page of pages.all) {
-  page.addEventListener("click", () => page.classList.toggle(Classes.PAGE_WRAPPED));
-}
+burger.onclick = () => {
+  // toggling burger state
+  burger.classList.toggle(Classes.BURGER_ACTIVE);
+
+  // toggling current page state
+  pages.current.classList.toggle(Classes.PAGE_WRAPPED);
+};
